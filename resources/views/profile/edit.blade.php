8d750736dd75d2f7aa5a33ebf8d5d9ef8877b5f4
Rafli Ramadhani <rafliramadhani438@gmail.com>
2025-04-20 22:38:01 +0700

    admin & hrd just have update password

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -13,38 +13,39 @@
     <div class="py-2">
         <div class="space-y-6 sm:space-y-3">
 
-            <div class="grid-cols-4 space-y-5 py-4 md:grid md:gap-x-6 md:space-y-0">
-                <div class="col-span-2 flex flex-col justify-start">
-                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                        {{ __('Personal Account') }}
-                    </h2>
+            @if (!Auth::user()->is_admin && !Auth::user()->is_hrd)
+                <div class="grid-cols-4 space-y-5 py-4 md:grid md:gap-x-6 md:space-y-0">
+                    <div class="col-span-2 flex flex-col justify-start">
+                        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                            {{ __('Personal Account') }}
+                        </h2>
 
-                    <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                        {{ __('Update your personal account information') }}
-                    </p>
+                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                            {{ __('Update your personal account information') }}
+                        </p>
+                    </div>
+
+                    <div class="col-span-2 flex justify-center">
+                        <livewire:profile.update-personal-account-form />
+                    </div>
                 </div>
 
-                <div class="col-span-2 flex justify-center">
-                    <livewire:profile.update-personal-account-form />
+                <div class="grid-cols-4 space-y-5 py-4 md:grid md:gap-x-6 md:space-y-0">
+                    <div class="col-span-2 flex flex-col justify-start">
+                        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                            {{ __('Personal Information') }}
+                        </h2>
+
+                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                            {{ __('Update your personal information') }}
+                        </p>
+                    </div>
+
+                    <div class="col-span-2 flex justify-center">
+                        <livewire:profile.update-personal-information-form />
+                    </div>
                 </div>
-            </div>
-
-            <div class="grid-cols-4 space-y-5 py-4 md:grid md:gap-x-6 md:space-y-0">
-                <div class="col-span-2 flex flex-col justify-start">
-                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                        {{ __('Personal Information') }}
-                    </h2>
-
-                    <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                        {{ __('Update your personal information') }}
-                    </p>
-                </div>
-
-                <div class="col-span-2 flex justify-center">
-                    <livewire:profile.update-personal-information-form />
-                </div>
-            </div>
-
+            @endif
 
             <div class="grid-cols-4 space-y-5 py-4 md:grid md:gap-x-6 md:space-y-0">
 
@@ -64,9 +65,9 @@
 
             </div>
 
-            <div class="items-end justify-end py-4 md:flex">
+            {{-- <div class="items-end justify-end py-4 md:flex">
 
-                <div class="rounded-xl border-2 border-red-600 p-4 dark:border-red-700 md:max-w-lg xl:max-w-xl">
+                <div class="p-4 border-2 border-red-600 rounded-xl dark:border-red-700 md:max-w-lg xl:max-w-xl">
                     <h2 class="text-xl font-medium text-red-700">
                         {{ __('Delete Account') }}
                     </h2>
@@ -80,7 +81,7 @@
                     </div>
                 </div>
 
-            </div>
+            </div> --}}
         </div>
     </div>
 
